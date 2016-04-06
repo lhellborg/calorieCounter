98f10b64642d9cb46d45a00f908f2814de38825d
Linda Hellborg <linda.hellborg@gmail.com>
2016-04-06 13:19:43 +0200

    upstart

--- js/views/food.js
+++ js/views/food.js
@@ -4,43 +4,52 @@ var FoodView = Backbone.View.extend({
 
 	events: {
       'click button#addItem': 'addItem',
-      'keyup input#food': 'search'
+      'keyup input#food': 'search',
     },
 
 	initialize: function() {
 		_.bindAll(this, 'render', 'addItem', 'search');
 
 		this.foodCollection = this.collection;
-		this.searchableCollection = new SearchableCollection;
-		this.searchableCollection.add(ham);
-		this.searchableCollection.add(butter);
-		this.searchableCollection.add(sushi);
-		this.searchableCollection.add(hamster);
-		this.render();
+
 	},
 
 	render: function() {
-		alert("Add a food item")
+			$('#showSearch').empty();
+			var ul = $('<ul id="foodFilter"></ul>');
+			$('#showSearch').append(ul);
+			var that = this;
+			this.searchableCollection.forEach(function(item) {
+				ul.append(new FoodEntryView({model: item, collection: that.foodCollection}).render());
+			})
 	},
 
 	search: function() {
 		var searchstring = $('#food').val(); //the input string
-		var regExp = new RegExp(searchstring, "i")
-		var selected = []
-		this.searchableCollection.forEach(function(item) {
-			var string = item.get('foodItem') //a food item
-			var res = string.search(regExp); //an insensitive search
-			if (res !== -1) {
-				selected.push(item);
-			}
+
+		var posting = $.post('https://api.nutritionix.com/v1_1/search' , {
+			 "appId":"38be9a6b",
+			 "appKey":"7f1e5364fc8d5c22bfa241fd43987a29",
+			 "query": searchstring
 		});
-		console.log(selected);
-		return selected;
+		this.searchableCollection = new SearchableCollection();
+
+		var that = this;
+		posting.done(function(data) {
+
+		 	data.hits.forEach(function(item) {
+		 		that.searchableCollection.add({
+		 			foodItem: item.fields.item_name,
+		 			calories: 40
+		 		});
+		 	});
+		 	that.render();
+		});
 	},
 
 
 	// addItem(): Custom function called via click event above.
-    addItem: function(){
+    addItem: function(event){
 		var food = $('#food').val();
 		var calories = $('#calories').val();
 		var item = new Food({
@@ -50,4 +59,26 @@ var FoodView = Backbone.View.extend({
 		this.foodCollection.add(item); // add item to collection; view is updated via event 'add'
 
     }
+});
+
+var FoodEntryView = Backbone.View.extend({
+	tagName: 'li',
+
+	events: {
+		'click': 'selectItem'
+	},
+	initialize: function() {
+		_.bindAll(this, 'render', 'selectItem')
+	},
+	render: function() {
+		return this.$el.append(this.model.get("foodItem"));
+	},
+	selectItem: function() {
+		var item = new Food({
+			foodItem: this.model.get('foodItem'),
+			calories: this.model.get('calories')
+		});
+		this.collection.add(item);
+	}
+
 });
